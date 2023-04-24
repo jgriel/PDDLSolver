00d7d26fa65dd3066d89c82f05c0cca32b05a572
jemrey <jemrey@fandm.edu>
2023-04-24 14:34:58 -0400

    Started GraphGeneration.py

---
+++ GraphGeneration.py
@@ -0,0 +1,23 @@
+
+def expand(state, actions):
+
+    """Finds all the possible actions that can be taken 
+        from the current state.
+
+
+    Args:
+        state: state Dictionary containing the information of the state. 
+        actions: The possible actions from the domain file.
+
+    Returns:
+        list of states that are able to be reached from the current state
+        with one action taken
+
+    """
+
+    pass
+
+def match(predicates, actions):
+
+    pass
+
