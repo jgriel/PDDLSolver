d7f1335a3fcfe5ef0d979538449ee932ac8e32c2
noahfox555@gmail.com <noahfox555@gmail.com>
2023-04-26 14:38:24 -0400

    working on greedy

--- PDDLsolver.py
+++ PDDLsolver.py
@@ -3,6 +3,28 @@ import queue
 import DomainParser, ProblemParser, GraphGeneration
 
 # Use a visited
+'''
+A node representing a state of the problem
+state is a list of statements of the state [(True, has-ball, ["Noah"]), (True, in-room, ["Noah"])]
+totalDistance is the totalDistance from the start
+parent is a node representing the current nodes parent
+'''
+class Node:
+	def __init__(self, state, totalDistance, aTobDistance, parent):
+		self.totalDistance = totalDistance # f_n
+		self.state = state
+		self.aTobDistance = aTobDistance # g_n
+		self.parent = parent
+
+	def __str__(self):
+		return "(" + str(self.totalDistance) + ", " + self.state + ", " + str(self.aTobDistance) + ", " + ("None" if self.parent is None else str(self.parent.state)) + ")"
+	
+	def __gt__(self, other):
+		return (self.totalDistance, self.state, self.aTobDistance) > (other.totalDistance, other.state, other.aTobDistance)
+				
+	def __lt__(self, other):
+		return (self.totalDistance, self.state, self.aTobDistance) < (other.totalDistance, other.state, other.aTobDistance)
+
 
 def breadth_first_search(initial_state, goal_state, domain):
     pass
@@ -16,6 +38,11 @@ def depth_first_search(initial_state, goal_state, domain):
 Pick the state with the best hueristic 
 '''
 def greedy_best_first_search(initial_state, goal_state, domain):
+    cur_state = Node(initial_state, 0, 0, None)
+    print(cur_state)
+    visited = []
+
+    while (cur_state)
     pass
 
 
@@ -86,14 +113,15 @@ def goalcheck(cur_state, goal_state):
 def heuristic(cur_state, goal_state):
     # count number of missing values in goal state compared to cur state
     minimum_moves_remaining = 0
+    print(goal_state)
+
+    # check if the condition from the goal_state in cur_state
     for predicate_key in goal_state:
         for condition in goal_state[predicate_key]:
             goal_condition_present = (condition in cur_state[predicate_key])
             if (not goal_condition_present):
                 minimum_moves_remaining += 1
 
-    print(predicate_key)
-    
     return minimum_moves_remaining
 
 if __name__ == "__main__":
