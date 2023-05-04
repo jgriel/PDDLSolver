c2d0679c336da3ea0cdc9751d86380979eb470b5
jgriel <jgriel1@fandm.edu>
2023-05-03 22:21:04 -0400

    start of depth-first-search

--- PDDLsolver.py
+++ PDDLsolver.py
@@ -50,6 +50,17 @@ def breadth_first_search(initial_state, goal_state, domain, problem):
 
 
 def depth_first_search(initial_state, goal_state, domain, problem):
+    stack = []
+    # print("INIT:", initial_state[0], "\n")
+    # print("GOAL:", goal_state, "\n")
+    # print("DOMAIN:", domain.keys(), "\n")
+    # print("PROBLEM:", problem.keys(), "\n")
+    
+    cur_state = State_Node(initial_state)
+    visited = [cur_state.state]
+    expansion = GraphGeneration.expand(problem["objects"], cur_state.state, domain)
+    # print("1st EXPAND:", expansion[1], "\n")
+    
     pass
 
 
@@ -173,6 +184,7 @@ if __name__ == "__main__":
     # print(heuristic(problem_dict["init"], problem_dict["goal"]))
     greedy_best_first_search(problem_dict["state"], problem_dict["goal"], domain_dict, problem_dict)
     breadth_first_search(problem_dict['state'], problem_dict['goal'], domain_dict, problem_dict)
+    depth_first_search(problem_dict['state'], problem_dict['goal'], domain_dict, problem_dict)
     # how to call search function
     # depth_first_search(problem_dict["init"], problem_dict["goal"])
     
